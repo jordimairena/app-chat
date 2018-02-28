71d926a944078183b6da6f51891659b3363e1e09
guillelopez22 <guillelopez_13@hotmail.com>
2018-02-27 23:12:32 -0800

    creando autenticacion para los usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'chats/index'
+    root 'home#inicio'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registros'
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-%w(user trainer admin).each do |role|
+%w(users trainer admin).each do |role|
   Role.create name: role
 end
+User.create(primer_nombre: 'jordi', apellido: 'mairen', usuario: 'jordi', password: 'password123', email: 'jordi@email.com', role: Role.last)
